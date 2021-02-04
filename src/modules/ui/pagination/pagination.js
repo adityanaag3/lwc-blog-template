a53bb30f9f5d3d9345d6c56a60ddf9df0d46fe75
adityanaag3 <atopalli@salesforce.com>
2021-02-05 00:17:35 +0530

    Bug Fixes

--- src/modules/ui/pagination/pagination.js
+++ src/modules/ui/pagination/pagination.js
@@ -3,4 +3,12 @@ import { LightningElement, api } from 'lwc';
 export default class Pagination extends LightningElement {
     @api previous;
     @api next;
+
+    get showprevious() {
+        return this.previous && this.previous !== '.';
+    }
+
+    get shownext() {
+        return this.next && this.next !== '.';
+    }
 }
