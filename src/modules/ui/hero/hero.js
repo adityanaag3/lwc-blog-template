d52103d5f1dbab795444d8044bcd1326bb83239a
adityanaag3 <atopalli@salesforce.com>
2021-02-05 17:51:28 +0530

    Fixing broken urls for custom public path

--- src/modules/ui/hero/hero.js
+++ src/modules/ui/hero/hero.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Hero extends LightningElement {}
+export default class Hero extends LightningElement {
+    @api publicPath;
+
+    renderedCallback() {
+        this.template
+            .querySelector('.hero-bg')
+            .style.setProperty(
+                '--bgurl',
+                `url(${this.publicPath}resources/blog-bg.jpeg)`
+            );
+    }
+}
--- src/modules/ui/socialHandles/socialHandles.js
+++ src/modules/ui/socialHandles/socialHandles.js
@@ -1,10 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import config from '../../../../config/blog.js';
 
 export default class SocialHandles extends LightningElement {
+    @api publicPath;
+
     get socialHandles() {
-        return config.socialHandles.map(function (el) {
-            el.svgUrl = `/resources/social-icons/${el.name}.svg#${el.name}`;
+        return config.socialHandles.map((el) => {
+            el.svgUrl = `${this.publicPath}resources/social-icons/${el.name}.svg#${el.name}`;
             el.ariaLabel = `Icon for ${el.name}`;
             return el;
         });
